1db289d4d6afcbdfa2bc360442f656b8dcd86326
cc299792458 <cc2018011855@gmail.com>
2024-03-26 23:12:09 -0700

    forward process

--- diffusion_model/diffusion_model.py
+++ diffusion_model/diffusion_model.py
@@ -1,7 +1,68 @@
+import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+
+from dataset_utils import StanfordCars, show_images, tensor_to_PIL
+from network_utils import UNet
+
+def show_forward_process(image, diffusion_model, timesteps=200, num_images=10):
+    stepsize = int(timesteps / num_images)
+
+    plt.figure(figsize=(12, 12))
+    for i in range(0, timesteps, stepsize):
+        time = torch.Tensor([i]).type(torch.int64)
+        processed_image, noise = diffusion_model.forward_diffusion(image, time)
+        pil_image = tensor_to_PIL(processed_image)
+        plt.subplot(1, num_images + 1, i // stepsize + 1)
+        plt.imshow(pil_image)
+        plt.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+class DiffusionModel:
+    def __init__(self, unet, beta_start=0.0001, beta_end=0.02, timesteps=200, device='cpu'):
+        self.network = unet.to(device)
+        self.device = device
+        self.setup_noise_schedule(beta_start, beta_end, timesteps)
+
+    def setup_noise_schedule(self, beta_start, beta_end, timesteps):
+        betas = torch.linspace(beta_start, beta_end, steps=timesteps, device=self.device)
+        alphas = 1.0 - betas
+        alphas_cumprod = torch.cumprod(alphas, dim=0)
+        alphas_cumprod_prev = torch.cat([torch.tensor([1.0], device=self.device), alphas_cumprod[:-1]])
+
+        self.betas = betas
+        self.sqrt_alphas = torch.sqrt(alphas)
+        self.sqrt_alphas_cumprod = torch.sqrt(alphas_cumprod)
+        self.sqrt_one_minus_alphas_cumprod = torch.sqrt(1.0 - alphas_cumprod)
+        self.sqrt_reciprocals_alphas = torch.sqrt(1.0 / alphas)
+        self.posterior_variance = betas * (1.0 - alphas_cumprod_prev) / (1.0 - alphas_cumprod)
+
+    def forward_diffusion(self, image, timestep):
+        image = image.to(self.device)
+        noise = torch.randn_like(image)
+        
+        sqrt_alpha_cumprod = self.sqrt_alphas_cumprod[timestep]
+        sqrt_one_minus_alpha_cumprod = self.sqrt_one_minus_alphas_cumprod[timestep]
+        
+        noisy_image = sqrt_alpha_cumprod * image + sqrt_one_minus_alpha_cumprod * noise
+        
+        return noisy_image, noise
 
 
+if __name__ == '__main__':
+    ##### 1. Load dataset #####
+    dataset = StanfordCars()
+    dataloader = DataLoader(dataset, batch_size=128, shuffle=True, drop_last=True)
+    # show_images(dataset=dataset, num_samples=8)
 
+    ##### 2. Train the diffusion model(Forward process) #####
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    timesteps = 200
 
+    unet = UNet()
+    diffusion_model = DiffusionModel(unet=unet, device=device, timesteps=timesteps)
+    # show_forward_process(image=next(iter(dataloader))[0], diffusion_model=diffusion_model, timesteps=timesteps)
 
-
-
+    ##### 3. Evaluate the diffusion model(Reverse process) #####
--- diffusion_model/network_utils.py
+++ diffusion_model/network_utils.py
@@ -1,3 +1,126 @@
-class UNet:
-    def __init__(self):
-        pass
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import math
+
+#NOTE: Not sure this form is absolutly the same to the pos encoding in "Attention is All You Need"
+# Timesteps embedding
+class SinusoidalPositionEmbeddings(nn.Module):
+    def __init__(self, dim):
+        super(SinusoidalPositionEmbeddings, self).__init__()
+        
+        self.dim = dim
+
+    def forward(self, time):
+        # Calculate the scale
+        device = time.device
+        half_dim = self.dim // 2
+        scale = torch.exp(torch.arange(half_dim, dtype=torch.float32, device=device) * (-math.log(10000.0) / half_dim))
+        
+        # Calculate the time encodings
+        time = time.unsqueeze(1)
+        time_encodings = torch.zeros(time.size(0), self.dim, device=device)
+        time_encodings[:, 0::2] = torch.sin(time * scale)
+        time_encodings[:, 1::2] = torch.cos(time * scale)
+        
+        return time_encodings
+
+
+class UNetBlock(nn.Module):
+    def __init__(self, input_channels, output_channels, time_embedding_dim, is_upsample=False):
+        super(UNetBlock, self).__init__()
+        
+        # Time-dependent transformation
+        self.time_dense = nn.Linear(time_embedding_dim, output_channels)
+        
+        # Spatial transformation based on the operation mode (up or not)
+        if is_upsample:
+            self.initial_conv = nn.Conv2d(input_channels * 2, output_channels, kernel_size=3, padding=1)
+            self.spatial_transform = nn.ConvTranspose2d(output_channels, output_channels, kernel_size=4, stride=2, padding=1)
+        else:
+            self.initial_conv = nn.Conv2d(input_channels, output_channels, kernel_size=3, padding=1)
+            self.spatial_transform = nn.Conv2d(output_channels, output_channels, kernel_size=4, stride=2, padding=1)
+        
+        self.final_conv = nn.Conv2d(output_channels, output_channels, kernel_size=3, padding=1)
+        self.activation = nn.ReLU()
+        self.batch_norm = nn.BatchNorm2d(output_channels)
+
+    def forward(self, x, time_embedding):
+        # Apply initial spatial transformation
+        x = self.activation(self.batch_norm(self.initial_conv(x)))
+        
+        # Transform and integrate time information
+        time_features = self.activation(self.time_dense(time_embedding)).unsqueeze(-1).unsqueeze(-1)
+        x += time_features
+        
+        # Apply final spatial transformation and activate
+        x = self.activation(self.batch_norm(self.final_conv(x)))
+        
+        # Upsample or downsample the feature map
+        x = self.spatial_transform(x)
+        
+        return x
+
+
+class UNet(nn.Module):
+    def __init__(self, img_channels=3, img_size=64, out_channels=3):
+        super(UNet, self).__init__()
+        # Define image size, channels, and time embedding dimensions
+        self.img_size = img_size
+        time_embedding_dim = 32
+        
+        # Set the channel sizes for down and up sampling paths
+        down_channels = [64, 128, 256, 512, 1024]
+        up_channels = [1024, 512, 256, 128, 64]
+        
+        # Time embedding layer
+        self.time_embedding = nn.Sequential(
+            SinusoidalPositionEmbeddings(time_embedding_dim),
+            nn.Linear(time_embedding_dim, time_embedding_dim),
+            nn.ReLU()
+        )
+        
+        # Initial convolution layer to match the channel size of the first downsample layer
+        self.initial_conv = nn.Conv2d(img_channels, down_channels[0], kernel_size=3, padding=1)
+        
+        # Downsample layers
+        self.downsample_layers = nn.ModuleList([
+            UNetBlock(input_channels=down_channels[i], output_channels=down_channels[i+1], time_embedding_dim=time_embedding_dim, is_upsample=False)
+            for i in range(len(down_channels)-1)
+        ])
+        
+        # Upsample layers
+        self.upsample_layers = nn.ModuleList([
+            UNetBlock(input_channels=up_channels[i], output_channels=up_channels[i+1], time_embedding_dim=time_embedding_dim, is_upsample=True)
+            for i in range(len(up_channels)-1)
+        ])
+        
+        # Final convolution layer to convert the feature map back to the target channel size
+        self.final_conv = nn.Conv2d(up_channels[-1], out_channels, kernel_size=1)
+    
+    def forward(self, x, timestep):
+        # Embed the timestep
+        time_embed = self.time_embedding(timestep)
+        
+        # Process with initial convolution
+        x = self.initial_conv(x)
+        
+        # Downsample, saving the output of each layer for the subsequent upsample connection
+        skip_connections = []
+        for layer in self.downsample_layers:
+            x = layer(x, time_embed)
+            skip_connections.append(x)
+        
+        # Upsample, using the saved downsample layer outputs for skip connections
+        for layer, skip_connection in zip(self.upsample_layers, reversed(skip_connections)):
+            x = torch.cat((x, skip_connection), dim=1)  # Concatenate for skip connection
+            x = layer(x, time_embed)
+        
+        # Apply final convolution to produce the output
+        x = self.final_conv(x)
+        return x
+    
+if __name__ == '__main__':
+    model = UNet()
+    print(model)
+    print("Num params:", sum(p.numel() for p in model.parameters()))
