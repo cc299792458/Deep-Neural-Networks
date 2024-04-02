c85c7e6641e7367632b91e1288a6be7f939945a6
cc299792458 <cc2018011855@gmail.com>
2024-04-02 05:43:30 -0700

    vae

--- auto_encoder/variational_auto_encoder/vae.py
+++ auto_encoder/variational_auto_encoder/vae.py
@@ -1,7 +1,9 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 
 from torchvision.transforms import ToTensor
+from torchvision.transforms.functional import to_pil_image
 from torchvision.datasets import MNIST, CIFAR10
 from torch.utils.data import DataLoader
 
@@ -12,8 +14,9 @@ import os
 
 class VAE(AutoEncoder):
     def __init__(self, feature_size: int, config: dict = None, device: str = 'cpu', lr: float = 0.001, epochs: int = 20) -> None:
+        # self.config['latent_dim'] = self.config['latent_dim'] * 2   # mean and log_variance
         super().__init__(feature_size, config, device, lr, epochs)
-        self.config['latent_dim'] = self.config['latent_dim'] * 2   # mean and log_variance
+        
     
     def forward(self, x):
         mean, log_var = torch.chunk(self.encoder(x), 2, dim=1)  # Split the encoded values into mean and log_var components
@@ -40,6 +43,52 @@ class VAE(AutoEncoder):
         loss.backward()
         self.optimizer.step()
         return loss.item()
+    
+    def create_fc_encoder(self):
+        layers = []
+        input_size = self.feature_size
+        for hidden_size in self.config['hidden_sizes']:
+            layers.append(nn.Linear(input_size, hidden_size))
+            layers.append(nn.ReLU())
+            input_size = hidden_size
+        layers.append(nn.Linear(input_size, self.config['latent_dim'] * 2))     # mean and log_variance
+        return nn.Sequential(*layers)
+    
+    def create_cnn_encoder(self):
+        input_channels = self.config['channels']
+        image_size = self.config['image_size']
+        encoder_hidden_channels = self.config['encoder_hidden_channels']
+        encoder_kernel_sizes = self.config['encoder_kernel_sizes']
+        encoder_strides = self.config['encoder_strides']
+        encoder_paddings = self.config['encoder_paddings']
+        latent_dim = self.config['latent_dim'] * 2  # mean and log_variance
+
+        layers = []
+        in_channels = input_channels
+        for out_channels, kernel_size, stride, padding in zip(encoder_hidden_channels, encoder_kernel_sizes, encoder_strides, encoder_paddings):
+            layers += [
+                nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding),
+                nn.ReLU()
+            ]
+            in_channels = out_channels
+            image_size = (image_size - kernel_size + 2 * padding) // stride + 1
+
+        layers += [
+            nn.Flatten(),
+            nn.Linear(in_channels * image_size * image_size, latent_dim)
+        ]
+
+        self.reduced_size = image_size
+
+        return nn.Sequential(*layers)
+    
+    def reconstruct_and_save_image(self, sample, log_dir, channels, image_size, epoch=None):
+        epoch = 'init' if epoch is None else f'epoch_{epoch}'
+        with torch.no_grad():
+            reconstructed_sample = self.forward(sample)[0].reshape(channels, image_size, image_size)
+            reconstructed_image = to_pil_image(reconstructed_sample.cpu())
+            save_path = os.path.join(log_dir, f'reconstructed_image_{epoch}.png')
+            reconstructed_image.save(save_path)
 
 if __name__ == '__main__':
     ##### 0. Load Dataset #####
@@ -62,7 +111,7 @@ if __name__ == '__main__':
 
     feature_size = channels * image_size * image_size
     vae = VAE(feature_size=feature_size, device=device,
-                               config={'type': 'cnn', 
+                               config={'type': 'fc', 
                                        'hidden_sizes': [512, 256, 128, 64, 32],
                                        'channels': channels,
                                        'image_size': image_size,}).to(device)
