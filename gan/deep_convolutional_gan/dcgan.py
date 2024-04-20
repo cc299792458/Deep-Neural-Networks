b7427d999506f8278e5933be6cc23ad48e17f9b2
cc299792458 <cc2018011855@gmail.com>
2024-04-20 01:27:40 -0700

    dcgan

--- gan/deep_convolutional_gan/dcgan.py
+++ gan/deep_convolutional_gan/dcgan.py
@@ -39,18 +39,18 @@ class DCGAN(nn.Module):
             'image_size': 28,   # Default for MNIST
             'latent_dim': 128,
             ## For Fully Connected Network ##
-            'generator_hidden_sizes': [128, 256, 512, 1024],
-            'discriminator_hidden_sizes': [512, 256],
+            'g_hidden_sizes': [128, 256, 512, 1024],
+            'd_hidden_sizes': [512, 256],
             ## For Convolutional Network ##
-            'g_hidden_channels': [128, 128, 64],
-            'g_kernel_sizes': [4, 4, 3],
-            'g_strides': [2, 2, 1],
-            'g_paddings': [1, 1, 1],
+            'g_hidden_channels': [512, 256, 128, 64],
+            'g_kernel_sizes': [4, 4, 4, 4, 1],
+            'g_strides': [1, 2, 2, 2, 1],
+            'g_paddings': [0, 1, 1, 1, 2],
             'g_activation': nn.ReLU(),
-            'd_hidden_channels': [16, 32, 64, 128],
-            'd_kernel_sizes': [3, 3, 3, 3],
-            'd_strides': [2, 2, 2, 2],
-            'd_paddings': [1, 1, 1, 1],
+            'd_hidden_channels': [64, 128, 256],
+            'd_kernel_sizes': [4, 4, 4],
+            'd_strides': [2, 2, 2, 1],
+            'd_paddings': [1, 1, 2, 0],
             'd_activation': nn.LeakyReLU(0.2)
         }
 
@@ -64,8 +64,8 @@ class DCGAN(nn.Module):
         self.g_activation = self.config.get('g_activation')
         self.d_activation = self.config.get('d_activation')
 
-        self.generator = self.create_cnn_generator()
-        self.discriminator = self.create_cnn_discriminator()
+        self.generator = self.create_generator()
+        self.discriminator = self.create_discriminator()
         self.generator.apply(self.weights_init)
         self.discriminator.apply(self.weights_init)
 
@@ -74,53 +74,61 @@ class DCGAN(nn.Module):
         self.criterion = nn.BCELoss()
         self.epochs = epochs
 
-    def create_cnn_generator(self):
+    def create_generator(self):
         layers = []
-        init_size = self.image_size // 4   # 28 // 4 = 7
+        # init_size = self.image_size // 4   # 28 // 4 = 7
         hidden_channels = self.config['g_hidden_channels']
         kernel_sizes = self.config['g_kernel_sizes']
         strides = self.config['g_strides']
         paddings = self.config['g_paddings']
 
-        layers.append(nn.Linear(self.latent_dim, hidden_channels[0] * init_size * init_size))
-        # Seems like Unflatten can be replaced by a ConvTranspose2d
-        layers.append(nn.Unflatten(1, (hidden_channels[0], init_size, init_size)))
-        layers.append(nn.BatchNorm2d(hidden_channels[0]))
+        # layers.append(nn.Linear(self.latent_dim, hidden_channels[0] * init_size * init_size))
+        # layers.append(nn.Unflatten(1, (hidden_channels[0], init_size, init_size)))
+        # layers.append(nn.BatchNorm2d(hidden_channels[0]))
+        # layers.append(self.g_activation)
+        layers.append(nn.ConvTranspose2d(self.latent_dim, hidden_channels[0],
+                                             kernel_size=kernel_sizes[0], stride=strides[0],
+                                             padding=paddings[0]))
+        layers.append(nn.BatchNorm2d(num_features=hidden_channels[0]))
+        layers.append(self.g_activation)
         for i in range(len(hidden_channels) - 1):
             layers.append(nn.ConvTranspose2d(hidden_channels[i], hidden_channels[i+1],
-                                             kernel_size=kernel_sizes[i], stride=strides[i],
-                                             padding=paddings[i]))
+                                             kernel_size=kernel_sizes[i+1], stride=strides[i+1],
+                                             padding=paddings[i+1], bias=False))
             layers.append(nn.BatchNorm2d(num_features=hidden_channels[i+1]))
             layers.append(self.g_activation)
         layers.append(nn.ConvTranspose2d(in_channels=hidden_channels[-1], out_channels=self.channels,
                                          kernel_size=kernel_sizes[-1], stride=strides[-1], 
                                          padding=paddings[-1]))
-        layers.append(nn.BatchNorm2d(num_features=self.channels, momentum=0.8))
+        # layers.append(nn.BatchNorm2d(num_features=self.channels))
         layers.append(nn.Tanh())
 
         return nn.Sequential(*layers)
 
-    def create_cnn_discriminator(self):
+    def create_discriminator(self):
         layers = []
         hidden_channels = self.config['d_hidden_channels']
         kernel_sizes = self.config['d_kernel_sizes']
         strides = self.config['d_strides']
         paddings = self.config['d_paddings']
         in_channels = self.channels
-        image_size = self.image_size
+        # image_size = self.image_size
 
         for i in range(len(hidden_channels)):
             layers.append(nn.Conv2d(in_channels=in_channels, out_channels=hidden_channels[i],
                                     kernel_size=kernel_sizes[i], stride=strides[i],
-                                    padding=paddings[i]))
-            layers.append(nn.BatchNorm2d(num_features=hidden_channels[i], momentum=0.8))
+                                    padding=paddings[i], bias=False))
+            layers.append(nn.BatchNorm2d(num_features=hidden_channels[i]))
             layers.append(self.d_activation)
             # layers.append(nn.Dropout2d(0.25))
             in_channels = hidden_channels[i]
-            image_size = (image_size - kernel_sizes[i] + 2 * paddings[i]) // strides[i] + 1
-        final_num_features = hidden_channels[-1] * image_size * image_size
-        layers.append(nn.Flatten())
-        layers.append(nn.Linear(in_features=final_num_features, out_features=1))
+            # image_size = (image_size - kernel_sizes[i] + 2 * paddings[i]) // strides[i] + 1
+        # final_num_features = hidden_channels[-1] * image_size * image_size
+        # layers.append(nn.Flatten())
+        # layers.append(nn.Linear(in_features=final_num_features, out_features=1))
+        layers.append(nn.Conv2d(hidden_channels[-1], 1, 
+                                kernel_size=kernel_sizes[-1], stride=strides[-1], 
+                                padding=paddings[-1], bias=False))
         layers.append(nn.Sigmoid())
 
         return nn.Sequential(*layers)
@@ -136,7 +144,7 @@ class DCGAN(nn.Module):
     def learn(self, dataloader: DataLoader, log_dir=None):
         # Create batch of latent vectors that we will use to visualize
         # the progression of the generator
-        fixed_noise = torch.randn(64, self.latent_dim, device=self.device)
+        fixed_noise = torch.randn(64, self.latent_dim, 1, 1, device=self.device)
 
         # Establish convention for real and fake labels during training
         real_label = 1.
@@ -149,7 +157,6 @@ class DCGAN(nn.Module):
 
         print("Starting Training Loop...")
         for epoch in range(self.epochs):
-            model_saved_this_epoch = False
             for step, (real_image, _) in enumerate(dataloader):
                 real_image = real_image.to(self.device)
                 batch_size = real_image.shape[0]
@@ -198,9 +205,11 @@ class DCGAN(nn.Module):
                 model_path = os.path.join(models_dir, f'epoch_{epoch}_model.pth')
                 torch.save(self.state_dict(), model_path)
 
-        torch.save(self.state_dict(), os.path.join(log_dir, '/models/final_model.pth'))
-        self.plot_loss_curves(g_losses=g_losses, d_losses=d_losses)
-        self.visualize_progression(img_list=img_list, dataloader=dataloader)
+        models_dir = os.path.join(log_dir, 'models')
+        os.makedirs(models_dir, exist_ok=True)
+        torch.save(self.state_dict(), os.path.join(models_dir, 'final_model.pth'))
+        self.plot_loss_curves(g_losses=g_losses, d_losses=d_losses, log_dir=log_dir)
+        self.visualize_progression(img_list=img_list, dataloader=dataloader, log_dir=log_dir)
         
 
     def discriminator_step(self, real_image, real_label, fake_label, batch_size):
@@ -216,7 +225,7 @@ class DCGAN(nn.Module):
         real_score = output.mean().item()
 
         ## Train with all-fake batch
-        z = torch.randn(batch_size, self.latent_dim, device=self.device)
+        z = torch.randn(batch_size, self.latent_dim, 1, 1, device=self.device)
         gen_image = self.generator(z)
         fake_label = torch.full((batch_size, ), fake_label, dtype=torch.float, device=self.device)
         output = self.discriminator(gen_image.detach()).view(-1)
@@ -242,7 +251,7 @@ class DCGAN(nn.Module):
         
         return g_loss, fake_score_after_update   
     
-    def plot_loss_curves(self, g_losses, d_losses):
+    def plot_loss_curves(self, g_losses, d_losses, log_dir):
         """
             Loss versus training iteration
         """
@@ -253,24 +262,32 @@ class DCGAN(nn.Module):
         plt.xlabel("iterations")
         plt.ylabel("Loss")
         plt.legend()
-        plt.show()
 
-    def visualize_progression(self, img_list, dataloader):
+        loss_fig_path = os.path.join(log_dir, 'loss.png')
+        plt.savefig(loss_fig_path)
+
+        plt.show()
+        plt.close()
+
+    def visualize_progression(self, img_list, dataloader, log_dir):
         """
             Visualize the training progression of Generator with an animation.
         """
-        fig = plt.figure(figsize=(8,8))
+        fig = plt.figure(figsize=(8, 8))
         plt.axis("off")
         ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
         ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
 
+        ani_path = os.path.join(log_dir, 'progression.gif')
+        ani.save(ani_path, writer='imagemagick')
+        
         HTML(ani.to_jshtml())
 
         # Grab a batch of real images from the dataloader
         real_batch = next(iter(dataloader))
 
         # Plot the real images
-        plt.figure(figsize=(15, 15))
+        plt.figure(figsize=(15, 8))
         plt.subplot(1, 2, 1)
         plt.axis("off")
         plt.title("Real Images")
@@ -281,8 +298,11 @@ class DCGAN(nn.Module):
         plt.axis("off")
         plt.title("Gen Images")
         plt.imshow(np.transpose(img_list[-1],(1, 2, 0)))
+
+        comparision_fig_path = os.path.join(log_dir, 'comparision.png')
+        plt.savefig(comparision_fig_path)
+
         plt.show()
-    
 
 if __name__ == '__main__':
     set_seed()
@@ -312,7 +332,7 @@ if __name__ == '__main__':
 
     ## Training parameters ## 
     latent_dim = 128
-    epochs = 100
+    epochs = 1
 
     dcgan = DCGAN(feature_size=feature_size, device=device,
             config={'latent_dim': latent_dim, 
