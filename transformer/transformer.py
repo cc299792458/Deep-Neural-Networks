d9b531ffdd2e3986404dc288984d552d096fe39a
cc299792458 <cc2018011855@gmail.com>
2024-09-10 04:10:17 -0400

    position-wise feed forward

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -80,6 +80,25 @@ class ScaledDotProductAttention(nn.Module):
 
         return out, score
 
+class PositionwiseFeedForward(nn.Module):
+    """
+    "position-wise" because the FC layers are applied along the last d_model dimension
+    """
+    def __init__(self, d_model, hidden, drop_prob=0.1):
+        super(PositionalEncoding, self).__init__()
+        self.linear1 = nn.Linear(d_model, hidden)
+        self.linear2 = nn.Linear(hidden, d_model)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=drop_prob)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.linear2(x)
+
+        return x
+
 class Transformer:
     def __init__(self,
                  config=None) -> None:
